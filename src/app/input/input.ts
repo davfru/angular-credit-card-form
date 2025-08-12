dba53fe7c79d7237d545dba4419e59a2161a4e47
Davide Fruci <davide.fruci@gmail.com>
2025-08-12 17:03:40 +0000

    feat: add css to errors

--- src/app/input/input.ts
+++ src/app/input/input.ts
@@ -1,16 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './input.html',
   styleUrl: './input.css'
 })
 export class InputComponent {
-  
+
   @Input() control!: FormControl;
+  @Input() label!: string;
 
   constructor() {
+  }
+
+  showErrors() {
+    return this.control.dirty && this.control.touched && this.control.errors;
   }
 }
